6bf69433efdfbd3abae6b76154166594c7515661
Sofia <aleeexmr11@gmail.com>
2026-06-09 13:50:57 -0600

    optimize page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,12 @@
 import dynamic from "next/dynamic";
 
 import Hero from "@/components/hero/Hero";
+import Stars from "@/components/hero/Stars";
 
-const Stars = dynamic(
-  () => import("@/components/hero/Stars"),
-  {
-    ssr: false,
-  }
-);
-
-const About = dynamic(
-  () => import("@/sections/About"),
-  {
-    loading: () => null,
-  }
-);
-
-const Projects = dynamic(
-  () => import("@/sections/Projects"),
-  {
-    loading: () => null,
-  }
-);
-
-const Skills = dynamic(
-  () => import("@/sections/Skills"),
-  {
-    loading: () => null,
-  }
-);
-
-const Contact = dynamic(
-  () => import("@/sections/Contact"),
-  {
-    loading: () => null,
-  }
-);
+const About = dynamic(() => import("@/sections/About"));
+const Projects = dynamic(() => import("@/sections/Projects"));
+const Skills = dynamic(() => import("@/sections/Skills"));
+const Contact = dynamic(() => import("@/sections/Contact"));
 
 export default function Home() {
   return (
